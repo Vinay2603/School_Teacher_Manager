e3679f889938597a3fc0c36553f62451c21c3007
Vinay2603 <91530880+Vinay2603@users.noreply.github.com>
2022-02-18 22:02:45 +0530

    add

---
+++ backend/src/configs/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose")
+
+
+ module.exports = connect = ()=>{
+    return mongoose.connect("mongodb://127.0.0.1:27017/SchoolMagagment")
+}
+
+//module.exports = connect 
